6f65cb33f9b0b899fd418d1c6e48c2e248a39b8d
andriivna <farinaanna686@gmail.com>
2022-11-03 13:25:45 +0200

    lesson 4.1

---
+++ src/app/modules/user/services/resolvers/users.resolver.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {
+  Router, Resolve,
+  RouterStateSnapshot,
+  ActivatedRouteSnapshot
+} from '@angular/router';
+import { Observable, of } from 'rxjs';
+import {IUser} from "../../interfaces/user.interface";
+import {UserService} from "../user.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersResolver implements Resolve<IUser[]> {
+  constructor(private userService:UserService){
+
+  }
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IUser[]> {
+    return this.userService.getAll();
+  }
+}
